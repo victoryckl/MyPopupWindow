947a8fcc7ce9dad09cae41cfde01e76c0198b87d
victroyckl <victoryckl2012@gmail.com>
2013-05-31 23:30:19 +0800

    add popupwindow #	new file:   res/drawable/pop_bg.xml #	modified:   res/layout/activity_my_popup_window.xml #	new file:   res/layout/pop.xml #	modified:   src/com/example/mypopupwindow/MyPopupWindow.java #	new file:   src/com/example/pop/MyPop.java

--- src/com/example/mypopupwindow/MyPopupWindow.java
+++ src/com/example/mypopupwindow/MyPopupWindow.java
@@ -1,15 +1,29 @@
 package com.example.mypopupwindow;
 
+import com.example.pop.MyPop;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MyPopupWindow extends Activity {
 
+	private Button mBtnPopup;
+	private Display mDisplay;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_popup_window);
+		
+		mDisplay = getWindowManager().getDefaultDisplay();
+		
+		init();
 	}
 
 	@Override
@@ -19,4 +33,33 @@ public class MyPopupWindow extends Activity {
 		return true;
 	}
 
+	private void init() {
+		mBtnPopup = (Button)findViewById(R.id.button_popupwindow);
+		mBtnPopup.setOnClickListener(mBtnClickListener);
+	}
+	
+	private OnClickListener mBtnClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.button_popupwindow:
+				showPop();
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
+	
+	private void showPop() {
+		MyPop pop = new MyPop(this);
+		/*
+		 * popupWindow.showAsDropDown（View view）弹出对话框，位置在紧挨着view组件
+		 * showAsDropDown(View anchor, int xoff, int yoff)弹出对话框，位置在紧挨着view组件，x y 代表着偏移量
+		 * showAtLocation(View parent, int gravity, int x, int y)弹出对话框
+		 * parent 父布局 gravity 依靠父布局的位置如Gravity.CENTER  x y 坐标值
+		 */
+		pop.showAtLocation(getWindow().getDecorView(), Gravity.CENTER, 0, 0);
+	}
 }
---
+++ src/com/example/pop/MyPop.java
@@ -0,0 +1,99 @@
+package com.example.pop;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.text.InputType;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+
+import com.example.mypopupwindow.R;
+
+public class MyPop extends PopupWindow {
+
+	private static final String TAG = "MyPop";
+	private Context mContext;
+	
+	public MyPop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyPop(Context context) {
+		super(context);
+		initPopWindow(context);
+	}
+
+	public MyPop(View contentView) {
+		super(contentView);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyPop(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyPop(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyPop(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyPop(View contentView, int width, int height) {
+		super(contentView, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyPop(Context context, AttributeSet attrs, int defStyleAttr,
+			int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyPop(View contentView, int width, int height, boolean focusable) {
+		super(contentView, width, height, focusable);
+		// TODO Auto-generated constructor stub
+	}
+
+	void initPopWindow(Context context) {
+		Log.i(TAG, "initPopWindow");
+		if (context != null) {
+			mContext = context.getApplicationContext();
+			// 加载popupWindow的布局文件
+			View contentView = LayoutInflater.from(mContext).inflate(R.layout.pop, null);
+			// 设置popupWindow的背景颜色
+//			contentView.setBackgroundColor(Color.RED);
+			
+			setWidth(400);
+			setHeight(300);
+			// 为弹出框设定自定义的布局
+			setContentView(contentView);
+
+			final EditText editText = (EditText) contentView.findViewById(R.id.editText1);
+			// 设定当你点击editText时，弹出的输入框是啥样子的。这里设置默认为数字输入哦，这时候你会发现你输入非数字的东西是不行的哦
+			editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+			/*
+			 * 这个setFocusable(true);非常重要，如果不在弹出之前加上这条语句，你会很悲剧的发现，你是无法在
+			 * editText中输入任何东西的。该方法可以设定popupWindow获取焦点的能力。当设置为true时，系统会捕获到焦点给popupWindow
+			 * 上的组件。默认为false哦.该方法一定要在弹出对话框之前进行调用。
+			 */
+			setFocusable(true);
+		}
+	}
+	
+	@Override
+	public void showAtLocation(View parent, int gravity, int x, int y) {
+		
+		super.showAtLocation(parent, gravity, x, y);
+	}
+}
